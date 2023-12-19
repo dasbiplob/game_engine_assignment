079c207381393783c623327e378855f709cbd706
Biplob Das <biplob90.d@gmail.com>
2023-12-20 00:28:07 +0200

    Added the code comments and modified the files

--- src/main/java/fi/handson/gamengine/controller/GameAccountController.java
+++ src/main/java/fi/handson/gamengine/controller/GameAccountController.java
@@ -6,22 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+/**
+ * Controller class responsible for handling HTTP requests related to game accounts.
+ * Endpoints in this class are under the "/api/accounts" base path.
+ */
 @RestController
 @RequestMapping("/api/accounts")
 public class GameAccountController {
 
     private final GameAccountService gameAccountService;
+
+    /**
+     * Constructor-based dependency injection for GameAccountService.
+     *
+     * @param gameAccountService The service responsible for game account operations.
+     */
     @Autowired
     public GameAccountController(GameAccountService gameAccountService) {
         this.gameAccountService = gameAccountService;
     }
 
+    /**
+     * Handles HTTP POST requests to "/api/accounts/event" for triggering a game event.
+     *
+     * @param event The game event data sent in the request body.
+     * @return ResponseEntity containing the remaining balance after processing the event.
+     */
 
     @PostMapping ("/event")
     public ResponseEntity<Double> triggerEvent(@RequestBody GameEvent event) {
-        //chargeRequest.setEventId(chargeRequest.getPlayerId().concat(String.valueOf(System.currentTimeMillis())));
+        // Trigger the event and get the remaining balance from the GameAccountService.
         Double remainingBalance = gameAccountService.triggerEvent(event);
+        // Respond with the remaining balance in the HTTP response body.
         return ResponseEntity.ok(remainingBalance);
     }
 
--- src/main/java/fi/handson/gamengine/service/GameAccountService.java
+++ src/main/java/fi/handson/gamengine/service/GameAccountService.java
@@ -9,12 +9,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+/**
+ * Service class responsible for handling game account-related operations.
+ */
+
 @Service
 public class GameAccountService {
 
     private final CustomerRepository customerRepository;
     private final GameAccountRepository gameAccountRepository;
     private final GameEventRepository gameEventRepository;
+
+    /**
+     * Constructor for GameAccountService.
+     *
+     * @param customerRepository    Repository for managing customer-related data.
+     * @param gameAccountRepository Repository for managing game account-related data.
+     * @param gameEventRepository   Repository for managing game event-related data.
+     */
     @Autowired
     public GameAccountService(CustomerRepository customerRepository, GameAccountRepository gameAccountRepository, GameEventRepository gameEventRepository) {
         this.gameAccountRepository = gameAccountRepository;
@@ -22,21 +34,31 @@ public class GameAccountService {
         this.gameEventRepository = gameEventRepository;
     }
 
+    /**
+     * Triggers a game event and updates the account balance based on the event type.
+     *
+     * @param event The game event to be triggered.
+     * @return The updated account balance after processing the event.
+     */
     public Double triggerEvent(GameEvent event) {
+        // Find the customer ID associated with the player ID from the game account repository.
         String customerId = gameAccountRepository.findCustomerId(event.getPlayer_id());
+        // Find the current account balance of the customer.
         Double accountBalance = customerRepository.findAccountBalance(customerId);
+        // Validate the event type and save the event if it is a valid type (PURCHASE or WIN).
         if(StringUtils.isNotEmpty(event.getEvent_type())&&(event.getEvent_type()
                 .equalsIgnoreCase("PURCHASE")|| event.getEvent_type().equalsIgnoreCase("WIN")))
             gameEventRepository.save(event);
         else
             throw new RuntimeException("Event type is not proper");
-
+        // Update the account balance based on the event type and amount.
         if(StringUtils.trimToEmpty(event.getEvent_type()).equalsIgnoreCase("PURCHASE") && event.getAmount()<= accountBalance){
             accountBalance= accountBalance-event.getAmount();
 
         }else{
             accountBalance= accountBalance+event.getAmount();
         }
+        // Return the updated account balance.
         return accountBalance;
 
     }
